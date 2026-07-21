2179c82d31df2f1964f3fb5a6e4e56a7627b3602
marufjon27 <109968897+marufjon27@users.noreply.github.com>
2026-07-21 19:13:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 
-BOT_TOKEN = "8862982254:AAGjW1c7XfPJyXF_mCwhNVkNmy121vcPOFg"  # ⚠️ O'zingizning bot tokeningizni yozing
-SUPER_ADMIN_ID = 7385948496         # ⚠️ O'zingizning Telegram ID-ingizni yozing
+BOT_TOKEN = "8862982254:AAGjW1c7XfPJyXF_mCwhNVkNmy121vcPOFg"  
+SUPER_ADMIN_ID = 5846586605         # Avazbek
